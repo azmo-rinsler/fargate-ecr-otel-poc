4acb40d0c70f53c2729f1773184e0b76839089d6
Azmo Rinsler <azmo-rinsler@idexx.com>
2025-06-20 13:59:37 -0400

    troubleshooting

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ resource aws_ecs_cluster otel_cluster {
 }
 
 resource aws_cloudwatch_log_group otel_log_group {
-  name              = "otel-collector-logs"
+  name              = "fargate-ecr-otel-poc-logs"
   retention_in_days = 1
 }
 
